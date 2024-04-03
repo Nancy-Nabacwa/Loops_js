63e435539105ce3a7f8fed6af911865ab27153cc
Nancy-Nabacwa <nancypaulinenabacwa@gmail.com>
2024-04-03 15:11:12 +0300

    added javascript loops assignment

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ console.log(calculatedArray(arr))
 
 /* No.2 Write a program that takes in the following array and use a while loop 
 to iterate and break when the item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9]; */
-/*const breakAtFourth = (arrNum) =>{
+const breakAtFourth = (arrNum) =>{
       while(i<arrNum.length){
              if(i==4){
                  break;
